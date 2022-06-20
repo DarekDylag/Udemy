2912754dc3e0aa72514ead0643be4c6a3a49f3f3
Darek Legion <dylagdariusz@gmail.com>
2022-06-20 15:36:35 +0200

    bug nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('navbar')
+	const nav = document.querySelector('.navbar')
 
 	function addShadow() {
 		if (window.scrollY >= 300) {
